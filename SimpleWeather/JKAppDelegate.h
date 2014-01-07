7b197118dfdeea6e986617f340858c2f66787f18
Jeff Kempista <slj380@capitalone.com>
2014-01-07 10:03:10 -0500

    Initial Commit

---
+++ SimpleWeather/JKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JKAppDelegate.h
+//  SimpleWeather
+//
+//  Created by Kempista, Jeff on 1/7/14.
+//  Copyright (c) 2014 Jeff Kempista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Kempista, Jeff on 1/7/14.
+//  Copyright (c) 2014 Jeff Kempista. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JKAppDelegate class]));
+    }
+}
